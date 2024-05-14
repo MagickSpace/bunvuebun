50aece71ca676224c3ebd5885dc1d2f22f5d36e1
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2024-05-14 20:18:23 +0000

    ++

--- pull-locale-json.sh
+++ pull-locale-json.sh
@@ -10,6 +10,17 @@ create_json_file() {
 
 EN_FILE_PATH="src/i18n/locales/en.json"
 RU_FILE_PATH="src/i18n/locales/ru.json"
+DE_FILE_PATH="src/i18n/locales/de.json"
+NL_FILE_PATH="src/i18n/locales/nl.json"
+UK_FILE_PATH="src/i18n/locales/uk.json"
+ES_FILE_PATH="src/i18n/locales/es.json"
+FR_FILE_PATH="src/i18n/locales/fr.json"
+
 
 create_json_file "$EN_FILE_PATH"
 create_json_file "$RU_FILE_PATH"
+create_json_file "$DE_FILE_PATH"
+create_json_file "$NL_FILE_PATH"
+create_json_file "$UK_FILE_PATH"
+create_json_file "$ES_FILE_PATH"
+create_json_file "$FR_FILE_PATH"
